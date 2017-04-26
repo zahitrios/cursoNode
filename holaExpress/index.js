39c076177bd70414f7e37ed5bcbb76c4bb4a5677
Zahit <zahit@basico.fm>
2017-04-25 19:52:50 -0500

    sentencias, interpolacion, ciclos, ifs en jade

--- holaExpress/index.js
+++ holaExpress/index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set("view engine","jade")
 
 app.get('/', function (req, res) {
-  res.render('index',{"saludo":"Hola Zahit"});
+  res.render('index',{"saludo":"Hola Zahit","saludo2":"buenos días"});
 });
 
 
